8e82ebbe56a949005c0c79b44e9fdcab95335262
xy <2439325841@qq.com>
2024-06-25 10:08:10 +0800

    flutter create nb_app

---
+++ README.md
@@ -0,0 +1,10 @@
+### 起步
+
+- 安装配置好flutter环境
+
+- 创建一个flutter计数器模板
+
+- ```shell
+  flutter create nb_app
+  ```
+
---
+++ android/app/src/main/kotlin/com/example/nb_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nb_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
